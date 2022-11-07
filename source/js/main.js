8766d260d966815b70701d1e80880fd6563089e5
Timur <timurganiev.kzn@mail.ru>
2022-11-07 21:13:20 +0300

    fix bags

--- source/js/main.js
+++ source/js/main.js
@@ -44,3 +44,111 @@ window.addEventListener('DOMContentLoaded', () => {
 // breakpointChecker();
 
 // используйте .closest(el)
+
+// Modal
+
+const modalOpenButton = document.querySelector('.header-button');
+const modal = document.querySelector('.modal');
+const modalCloseButton = document.querySelector('.modal-button__close');
+const modalNameInput = document.querySelector('.modal-form__username-button');
+
+modalOpenButton.addEventListener('click', () => {
+  if (modal.classList.contains('visually-hidden')) {
+    modal.classList.remove('visually-hidden');
+    modalNameInput.focus();
+  }
+});
+
+modalCloseButton.addEventListener('click', () => {
+  modal.classList.add('visually-hidden');
+});
+
+function FormMasked() {
+  const maskedInputs = document.querySelectorAll('[data-mask]');
+
+  for (let i = 0; i < maskedInputs.length; i++) {
+    maskedInputs[i].addEventListener('input', maskInput);
+  }
+
+  for (let i = 0; i < maskedInputs.length; i++) {
+    maskedInputs[i].addEventListener('focusout', maskOutput);
+  }
+
+  function maskInput() {
+    let literalPattern = /[0\*]/;
+    let numberPattern = /[0-9]/;
+    let newValue = '';
+    let valueIndex = 0;
+    let maskIndex = 0;
+
+    while (maskIndex < this.dataset.mask.length) {
+      if (maskIndex >= this.value.length) {
+        break;
+      }
+      if (this.dataset.mask[maskIndex] === '0' && this.value[valueIndex].match(numberPattern) === null) {
+        break;
+      }
+      while (this.dataset.mask[maskIndex].match(literalPattern) === null) {
+        if (this.value[valueIndex] === this.dataset.mask[maskIndex]) {
+          break;
+        }
+        newValue += this.dataset.mask[maskIndex++];
+      }
+      newValue += this.value[valueIndex++];
+      maskIndex++;
+    }
+
+    this.value = newValue;
+  }
+
+  function maskOutput() {
+    if ((this.value.length - 7) < 11) {
+      this.value = '';
+    }
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  FormMasked();
+});
+
+
+//Footer drop menu
+
+const dropMenu = document.querySelectorAll('.footer-menu');
+const noJsFooterSections = document.querySelector('.footer-sections__list');
+const noJsFooterContacts = document.querySelector('.footer-contacts__list');
+
+noJsFooterSections.classList.remove('no--js');
+noJsFooterContacts.classList.remove('no--js');
+
+dropMenu.forEach((evt) =>
+  evt.addEventListener('click', () => {
+    if (evt.classList.contains('footer-menu__open')) {
+      evt.classList.remove('footer-menu__open');
+      evt.classList.add('footer-menu__closed');
+    } else {
+      dropMenu.forEach((event) => {
+        event.classList.remove('footer-menu__open');
+        event.classList.add('footer-menu__closed');
+      });
+      evt.classList.remove('footer-menu__closed');
+      evt.classList.add('footer-menu__open');
+    }
+  })
+);
+
+//About us drop menu
+
+let dropButton = document.querySelector('.about-us__info-button');
+let dropInfo = document.querySelector('.info-description__drop-info');
+
+dropButton.addEventListener('click', () => {
+  if (dropInfo.classList.contains('visually-hidden')) {
+    dropInfo.classList.remove('visually-hidden');
+    dropButton.textContent = 'Свернуть';
+  } else {
+    dropInfo.classList.add('visually-hidden');
+    dropButton.textContent = 'Подробнее';
+  }
+});
